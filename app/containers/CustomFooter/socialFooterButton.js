38408a8af5a2615397653666199e2c55787df35f
newmohib <newmohib@gmail.com>
2019-09-20 04:24:43 +0600

    remove some icom

--- app/containers/CustomFooter/socialFooterButton.js
+++ app/containers/CustomFooter/socialFooterButton.js
@@ -25,7 +25,6 @@ import {
   WeiboShareButton,
   PocketShareButton,
   InstapaperShareButton,
-
   FacebookIcon,
   TwitterIcon,
   LinkedinIcon,
@@ -48,115 +47,112 @@ import {
 import './socialButton.css';
 class SocialFooterButton extends Component {
   state = {};
+
   render() {
-      return (
-        <div className="Demo__container">
-          <div className="Demo__some-network">
-            <FacebookShareButton
-              url="https://www.facebook.com/"
-              quote="title"
-              className="Demo__some-network__share-button">
-              <FacebookIcon
-                size={32}
-                round />
-            </FacebookShareButton>
-            <TwitterShareButton
-              url="https://twitter.com/"
-              quote="title"
-              className="Demo__some-network__share-button"
+    return (
+      <div className="Demo__container">
+        <div className="Demo__some-network">
+          <FacebookShareButton
+            url="https://www.facebook.com/"
+            quote="title"
+            className="Demo__some-network__share-button"
           >
-              <TwitterIcon size={32} round />
-            </TwitterShareButton>
-
-            <TelegramShareButton
-              url="https://telegram.org/"
-              quote="title"
-              className="Demo__some-network__share-button"
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+          <TwitterShareButton
+            url="https://twitter.com/"
+            quote="title"
+            className="Demo__some-network__share-button"
           >
-              <TelegramIcon size={32} round />
-            </TelegramShareButton>
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
 
-            <WhatsappShareButton
-              url="https://www.whatsapp.com/"
-              quote="title"
-              separator=":: "
-              className="Demo__some-network__share-button"
+          <TelegramShareButton
+            url="https://telegram.org/"
+            quote="title"
+            className="Demo__some-network__share-button"
           >
-              <WhatsappIcon size={32} round />
-            </WhatsappShareButton>
+            <TelegramIcon size={32} round />
+          </TelegramShareButton>
 
-            <LinkedinShareButton
-              url="https://www.linkedin.com/"
-              quote="title"
-              windowWidth={750}
-              windowHeight={600}
-              className="Demo__some-network__share-button"
+          <WhatsappShareButton
+            url="https://www.whatsapp.com/"
+            quote="title"
+            separator=":: "
+            className="Demo__some-network__share-button"
           >
-              <LinkedinIcon size={32} round />
-            </LinkedinShareButton>
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
 
-            <TumblrShareButton
-              url="https://www.tumblr.com/"
-              quote="title"
-              windowWidth={660}
-              windowHeight={460}
-              className="Demo__some-network__share-button"
+          <LinkedinShareButton
+            url="https://www.linkedin.com/"
+            quote="title"
+            windowWidth={750}
+            windowHeight={600}
+            className="Demo__some-network__share-button"
           >
-              <TumblrIcon size={32} round />
-            </TumblrShareButton>
-            <LivejournalShareButton
-              url="https://www.livejournal.com/"
-              quote="title"
-              description=""
-              className="Demo__some-network__share-button"
-            >
-            </LivejournalShareButton>
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
 
-            <MailruShareButton
-              url=""
-              quote="title"
-              className="Demo__some-network__share-button"
+          <TumblrShareButton
+            url="https://www.tumblr.com/"
+            quote="title"
+            windowWidth={660}
+            windowHeight={460}
+            className="Demo__some-network__share-button"
           >
-              <MailruIcon size={32} round />
-            </MailruShareButton>
+            <TumblrIcon size={32} round />
+          </TumblrShareButton>
+          <LivejournalShareButton
+            url="https://www.livejournal.com/"
+            quote="title"
+            description=""
+            className="Demo__some-network__share-button"
+          />
 
-            <EmailShareButton
-              url="https://accounts.google.com/"
-              quote="title"
-              body="body"
-              className="Demo__some-network__share-button"
+          <MailruShareButton
+            url=""
+            quote="title"
+            className="Demo__some-network__share-button"
           >
-              <EmailIcon size={32} round />
-            </EmailShareButton>
+            <MailruIcon size={32} round />
+          </MailruShareButton>
 
-            
-
-            <WorkplaceShareButton
-              url="https://www.facebook.com/workplace"
-              quote="title"
-              className="Demo__some-network__share-button"
+          <EmailShareButton
+            url="https://accounts.google.com/"
+            quote="title"
+            body="body"
+            className="Demo__some-network__share-button"
           >
-              <WorkplaceIcon size={32} round />
-            </WorkplaceShareButton>
+            <EmailIcon size={32} round />
+          </EmailShareButton>
 
-            <PocketShareButton
-              url="https://getpocket.com/"
-              quote="title"
-              className="Demo__some-network__share-button"
+          <WorkplaceShareButton
+            url="https://www.facebook.com/workplace"
+            quote="title"
+            className="Demo__some-network__share-button"
           >
-              <PocketIcon size={32} round />
-            </PocketShareButton>
+            <WorkplaceIcon size={32} round />
+          </WorkplaceShareButton>
 
-            <InstapaperShareButton
-              url="https://www.instapaper.com/"
-              quote="title"
-              className="Demo__some-network__share-button"
+          <PocketShareButton
+            url="https://getpocket.com/"
+            quote="title"
+            className="Demo__some-network__share-button"
           >
-              <InstapaperIcon size={32} round />
-            </InstapaperShareButton>
+            <PocketIcon size={32} round />
+          </PocketShareButton>
+
+          <InstapaperShareButton
+            url="https://www.instapaper.com/"
+            quote="title"
+            className="Demo__some-network__share-button"
+          >
+            <InstapaperIcon size={32} round />
+          </InstapaperShareButton>
         </div>
-        </div>
-      );
+      </div>
+    );
   }
 }
 
